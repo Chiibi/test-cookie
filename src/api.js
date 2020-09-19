2b3548ac2f37e88ef714556ef529d67885537574
Chiibi <Chiibi.jame@gmail.com>
2020-09-19 20:17:11 +0700

    Fix front

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const serverless = require("serverless-http");
 const cookieParser = require('cookie-parser');
+const fs = require('fs')
 
 const app = express();
 const router = express.Router();
@@ -14,21 +15,22 @@ router.get("/check", (req, res) => {
 
 router.get("/ios14", (req, res) => {
   var cookie = req.cookies.cookieName;
+  
   if (cookie === undefined) {
     // no: set a new cookie
     var randomNumber=Math.random().toString();
     randomNumber=randomNumber.substring(2,randomNumber.length);
-    res.cookie('HereUrCookie',randomNumber, { maxAge: 900000, httpOnly: true });
+    res.cookie('HereUrCookie',randomNumber, { maxsAge: 900000, httpOnly: false });
     console.log('cookie created successfully');
-    res.send(`Cookie set ${cookie}`)
+    res.sendFile('/index.html', { root: path.join(process.cwd(), 'src') })
+    
   } else {
-    // yes, cookie was already present 
+    // yes, cookie was already present s
     console.log('cookie exists', cookie);
-    res.send(`Cookie exists ${cookie}`)
+    res.sendFile('/index.html', { root: path.join(process.cwd(), 'src') })
   }
 });
 
 app.use(`/.netlify/functions/api`, router);
 
-module.exports = app;
 module.exports.handler = serverless(app);
